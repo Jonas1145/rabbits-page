f5b9f7f69410d7ea25d555012ee25880398bd5f0
u27e51_avl <u27e51@DERGBWN111133.avl01.avlcorp.lan>
2025-01-24 18:20:08 +0100

    add imgs and setup header

--- src/Mainpage.tsx
+++ src/Mainpage.tsx
@@ -1,3 +1,17 @@
+import roundnetImage from './assets/roundnet_1.jpg'
 export default function Mainpage() {
-  return <div className="text-3xl">hello</div>
+  return (
+    <div className="flex flex-col md:flex-row items-center gap-4">
+      <div className="md:w-1/2">
+        <h2 className="text-green-600">Willkommen bei Roundnet-Rabbits Regensburg</h2>
+        <p>
+          Roundnet, im Volksmund auch bekannt unter dem Namen "Spikeball", ist eine Trendsportart
+          aus den USA, welche nun auch seinen Weg in die Donaustadt gefunden hat.
+        </p>
+      </div>
+      <div className="md:w-1/2">
+        <img src={roundnetImage} alt="" className="w-full h-auto" />
+      </div>
+    </div>
+  )
 }
